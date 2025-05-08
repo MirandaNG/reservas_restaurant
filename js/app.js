ee95c81ee3a090d7839e3502070d425d6d85d795
josue cornejo <jg.cornejourbina@ugto.mx>
2025-05-08 12:21:16 -0600

    Agregue funcionalidad a los botones de reserva y de la barra de navegación. Tambien cree las paginas login y registrar.

---
+++ js/app.js
@@ -0,0 +1,26 @@
+function seleccionarMesa(tipoMesa)
+{
+    window.location.href = "login.html"; //cambia a la pagina de las reservas
+    switch (tipoMesa) { //dependiendo el tipo de mesa se guarda un valor diferente
+        
+        case 0:
+            //Mesa para 2 personas
+            alert("Mesa para 2 personas seleccionada");
+            break;
+        case 1:
+            //Mesa para 4 personas
+            alert("Mesa para 4 personas seleccionada");
+            break;
+        case 2:
+            //Mesa para 6 personas
+            alert("Mesa para 6 personas seleccionada");
+            break;
+        case 3:
+            //Mesa para 8 personas
+            alert("Mesa para 8 personas seleccionada");
+            break;
+        default:
+            alert("Mesa no válida seleccionada");
+            break;
+    }
+}
