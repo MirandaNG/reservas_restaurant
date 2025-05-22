f1e6621bac5102eae7bf8d1ad1fe1f992a0edc4e
josue cornejo <jg.cornejourbina@ugto.mx>
2025-05-21 22:59:58 -0600

    Agregue tabla de reservaciones que se actualiza con la base de datos, tambien cree la ventana emergente para poder editar la reservacion y hice que funcionara. Corregi un error de la tabla, ya funciona

--- js/funcionesFirebase.js
+++ js/funcionesFirebase.js
@@ -1,6 +1,6 @@
 import { cerrarModal } from "./adminReservas.js";
 import db from "./firebaseConfig.js";
-import { addDoc, collection, where, query, getDocs, doc, updateDoc, getDoc, doc, updateDoc } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+import { addDoc, collection, where, query, getDocs, doc, updateDoc, getDoc} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-storage.js";
 //nota es importante instalar firebase en la raiz del proyecto
 //npm install firebase
